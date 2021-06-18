348154177568e19a324dadb08ea72f99570c8caa
filarty <filarty@gmail.com>
2021-06-18 18:07:50 +0400

    Classes update and new logic for button +++++ Check winning combinations ++++

--- GUI.py
+++ GUI.py
@@ -1,30 +1,32 @@
 import tkinter as tk
+import classes
 
 root = tk.Tk()
 root.title("Крестики-нолики")
 root.geometry("500x500")
 root.resizable(width=False, height=False)
 
-b1 = tk.Button(root, text="hello")
-b2 = tk.Button(root, text="hello")
-b3 = tk.Button(root, text="hello")
-b4 = tk.Button(root, text="hello")
-b5 = tk.Button(root, text="hello")
-b6 = tk.Button(root, text="hello")
-b7 = tk.Button(root, text="hello")
-b8 = tk.Button(root, text="hello")
-b9 = tk.Button(root, text="hello")
+
+b1 = classes.Button(root)
+b2 = classes.Button(root)
+b3 = classes.Button(root)
+b4 = classes.Button(root)
+b5 = classes.Button(root)
+b6 = classes.Button(root)
+b7 = classes.Button(root)
+b8 = classes.Button(root)
+b9 = classes.Button(root)
 
 
-b1.grid(row=0, column=0, stick="wens")
-b2.grid(row=0, column=1, stick="wens")
-b3.grid(row=0, column=2, stick="wens")
-b4.grid(row=1, column=0, stick="wens")
-b5.grid(row=1, column=1, stick="wens")
-b6.grid(row=1, column=2, stick="wens")
-b7.grid(row=2, column=0, stick="wens")
-b8.grid(row=2, column=1, stick="wens")
-b9.grid(row=2, column=2, stick="wens")
+b1.place(row=0, column=0)
+b2.place(row=0, column=1)
+b3.place(row=0, column=2)
+b4.place(row=1, column=0)
+b5.place(row=1, column=1)
+b6.place(row=1, column=2)
+b7.place(row=2, column=0)
+b8.place(row=2, column=1)
+b9.place(row=2, column=2)
 
 root.grid_columnconfigure(0, minsize=166)
 root.grid_columnconfigure(1, minsize=166)
@@ -33,4 +35,5 @@ root.grid_rowconfigure(0, minsize=170)
 root.rowconfigure(1, minsize=166)
 root.rowconfigure(2, minsize=166)
 
-tk.mainloop()
+if __name__ == "__main__":
+    tk.mainloop()
